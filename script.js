1a77ca7bad0530c75ac9c423c3ede78f31126d2b
Arslan Iqbal <arslaniqbal4666@gmail.com>
2025-12-19 13:12:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".multiple-text", {
+    strings:["Front-end-Developer","UI/UX Designer","Wordpress Designer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
